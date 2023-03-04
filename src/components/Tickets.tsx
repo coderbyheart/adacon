a2f870091d64572932e8cb79de21c129402e8962
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-04 17:26:48 +0100

    fix(tickets): typo

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -7,7 +7,7 @@ export const Tickets = () => (
 			<div class="row pb-lg-5">
 				<div class="col text-center">
 					<p>Ada Con Norway 2023 attendance will be free.</p>
-					<p>The registration open on the 1st of September 2023.</p>
+					<p>The registration opens on the 1st of September 2023.</p>
 					<p>
 						If you want to get notified once the ticket registration opens,{' '}
 						<a href="https://forms.gle/xZUiepTYBAbmV49u8" target="_blank">
