5a5b4420452a0008f1ca541ca1e62743d4d72d01
Markus Tacker <m@coderbyheart.com>
2023-03-05 11:06:02 +0100

    fix(stats): improve layout on mobile

--- src/components/ConfStats.tsx
+++ src/components/ConfStats.tsx
@@ -5,14 +5,14 @@ export const ConfStats = () => (
 	<div class="bg-highlight">
 		<div class="container">
 			<div class="row text-center text-white py-4">
-				<div class="offset-3 col-2">
+				<div class="col-4 offset-md-3 col-md-2">
 					<strong class="fs-1 fw-semibold font-headline">
 						{Con.statistics.attendees}
 					</strong>
 					<br />
 					<span class="fs-md-4 opacity-65 fw-semibold">Attendees</span>
 				</div>
-				<div class="col-2">
+				<div class="col-4 col-md-2">
 					<strong class="fs-1 fw-semibold font-headline">
 						{Con.statistics.days}
 					</strong>
@@ -25,7 +25,7 @@ export const ConfStats = () => (
 						/>
 					</span>
 				</div>
-				<div class="col-2">
+				<div class="col-4 col-md-2">
 					<strong class="fs-1 fw-semibold font-headline">
 						{Con.statistics.talks}
 					</strong>
