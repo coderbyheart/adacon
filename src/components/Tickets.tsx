cd8056e4e829736c6e93ffbfada4e108122e6b81
Markus Tacker <m@coderbyheart.com>
2023-09-04 09:28:26 +0200

    feat: add ticket link

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -1,23 +1,30 @@
 export const Tickets = () => (
 	<section id="tickets" class="mt-4 bg-highlight text-white">
-		<div class="container mt-4">
+		<div class="container mt-4 py-4">
 			<div class="row text-center py-4">
 				<h2 class="py-4">Tickets</h2>
 			</div>
 			<div class="row pb-lg-5">
 				<div class="col text-center">
-					<p>AdaCon Norway 2023 attendance will be free.</p>
-					<p>The registration opens on the 4th of September 2023.</p>
+					<p>AdaCon Norway 2023 attendance is free.</p>
 					<p>
-						If you want to get notified once the ticket registration opens,{' '}
+						We have only 200 seats,
+						<br />
+						so{' '}
+						<strong>
+							please only register for tickets you will actually use
+						</strong>
+						!
+					</p>
+					<p>
 						<a
-							href="https://forms.gle/xZUiepTYBAbmV49u8"
+							href="https://tikkio.com/tickets/40811-adacon-norway-2023"
 							target="_blank"
 							rel="noreferrer noopener"
+							class="btn btn-success"
 						>
-							please fill out this form
+							Register here
 						</a>
-						.
 					</p>
 				</div>
 			</div>
