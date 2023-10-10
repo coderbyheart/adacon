fa6032ecedbabe7dc22d2e1eb03b868ec243093a
Markus Tacker <m@coderbyheart.com>
2023-10-10 10:49:16 +0200

    fix(live-stream): add YouTube video

--- src/components/LiveStream.tsx
+++ src/components/LiveStream.tsx
@@ -1,17 +1,20 @@
+import { ViewportObserver } from 'preact-intersection-observer'
+
 export const LiveStream = () => (
 	<section class="py-4  bg-highlight text-white" id="live">
 		<div class="container text-center">
 			<div class="row">
-				<div class="col-md-6 offset-md-3 pt-4 pb-4">
+				<div class="col-md-8 offset-md-2 pt-4 pb-4">
 					<h2 class="fs-4 py-4">Live Stream</h2>
+					<EmbedVideo video="rt_eM_KRfK4" />
 					<p>
-						You can{' '}
+						You can watch the live stream{' '}
 						<a
 							href="https://vimeo.com/event/3782646"
 							target="_blank"
 							rel="nofollow noreferrer noopener"
 						>
-							watch the live stream here
+							on Vimeo here
 						</a>
 						.
 					</p>
@@ -19,4 +22,26 @@ export const LiveStream = () => (
 			</div>
 		</div>
 	</section>
+)
+
+const EmbedVideo = ({ video }: { video: string }) => (
+	<ViewportObserver
+		render={({ inView, entry }) => {
+			if (!inView) return null
+			const width = entry?.boundingClientRect.width ?? 560
+			const height = (width / 560) * 315
+			return (
+				<iframe
+					width={width}
+					height={height}
+					src={`https://www.youtube-nocookie.com/embed/${video}`}
+					title={'Live stream'}
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowFullScreen
+				/>
+			)
+		}}
+		options={{ triggerOnce: true }}
+	/>
 )
