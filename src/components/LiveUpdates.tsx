03041fa49830dbc2d6eed4a31a7152a6009089d3
Markus Tacker <m@coderbyheart.com>
2023-10-10 10:42:05 +0200

    style: no return

--- src/components/LiveUpdates.tsx
+++ src/components/LiveUpdates.tsx
@@ -1,20 +1,18 @@
-export const LiveUpdates = () => {
-	return (
-		<div
-			class="d-flex align-items-center"
-			style={{
-				backgroundColor: '#e8ff67',
-				height: '64px',
-			}}
-		>
-			<aside class="container">
-				<div class="row">
-					<div class="col">
-						Follow <a href="https://oslo.town/@adacon">@adacon@oslo.town</a> for
-						live-updates.
-					</div>
+export const LiveUpdates = () => (
+	<div
+		class="d-flex align-items-center"
+		style={{
+			backgroundColor: '#e8ff67',
+			height: '64px',
+		}}
+	>
+		<aside class="container">
+			<div class="row">
+				<div class="col">
+					Follow <a href="https://oslo.town/@adacon">@adacon@oslo.town</a> for
+					live-updates.
 				</div>
-			</aside>
-		</div>
-	)
-}
+			</div>
+		</aside>
+	</div>
+)
